ade909ba8657b0b9612a4df206afcec135eb153a
Toni Kamula <protoni91@gmail.com>
2019-12-26 19:32:53 +0200

    Handle API and SECRET KEYs in config files.

--- bot.py
+++ bot.py
@@ -1,23 +1,51 @@
 import requests, json
 import utils
 
+# Configs
+CONFIG_FILE = "config"
+API_KEY = utils.DEFAULT_API_KEY
+SECRET_KEY = utils.DEFAULT_SECRET_KEY
+
+# API endpoints
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
 ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 ASSETS_URL = "{}/v2/assets".format(BASE_URL)
-#HEADERS={'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY':SECRET_KEY}
+HEADERS={'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY':SECRET_KEY}
 
 ASSETS_FILE = "assets.txt"
-CONFIG_FILE = "config"
-
-API_KEY = "default"
-SECRET_KEY = "default"
+ASSETS = {}
+TRADEABLE_ASSETS = []
 
 def load_config():
-    print(utils.read_file(CONFIG_FILE))
+    global API_KEY, SECRET_KEY, HEADERS
+    data = utils.read_file(CONFIG_FILE)
+    splitted = data.split('\n')
+    
+    # Load API KEY
+    key = utils.get_value_from_config(CONFIG_FILE, "API_KEY")
+    if key != "" and key != utils.DEFAULT_API_KEY:
+        API_KEY = key
+    else:
+        print("Failed to load API KEY!")
+        
+    # Load Secret key
+    key = utils.get_value_from_config(CONFIG_FILE, "SECRET_KEY")
+    if key != "" and key != utils.DEFAULT_SECRET_KEY:
+        SECRET_KEY = key
+    else:
+        print("Failed to load SECRET KEY!")
+    
+    # Set config info to headers
+    HEADERS={'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY':SECRET_KEY}
 
 def load_assets():
-    pass
+    global ASSETS
+    
+    data = utils.read_file(ASSETS_FILE)
+    print(data)
+    ASSETS = data
+    
 
 def check_config():
     if not utils.file_exists(CONFIG_FILE):
@@ -31,21 +59,46 @@ def check_config():
         print("Config file OK")
         load_config()
 
+def print_all_stock_names():
+    global ASSETS
+    '''
+    for item in ASSETS:
+        print(item["symbol"])
+    '''
+    print("All assets: " + str(len(ASSETS)))
+    
+def print_tradeable_stock_names():
+    global TRADEABLE_ASSETS, ASSETS
+    
+    for item in ASSETS:
+        if item["tradable"]:
+            TRADEABLE_ASSETS.append(item)
+        
+    print("Tradeable assets: " + str(len(TRADEABLE_ASSETS)))
+
 def save_assets():
+    global ASSETS
+    
+    assets = get_assets()
+    ASSETS = assets;
+    
+    '''
     if not utils.file_exists(ASSETS_FILE):
     
         print("Fetching trading assets.")
         assets = get_assets()
         
+        ASSETS = assets;
         print("Saving trading assets.")
         utils.write_file("assets.txt", str(assets), False)
         
         print("Trading assets saved to " + ASSETS_FILE)
     else:
-        print("Trading assets OK")
+        print("Loading trading assets from the file system")
         load_assets()
+        print("Trading assets OK")
+    '''
 
-'''
 def get_account():
     r = requests.get(ACCOUNT_URL, headers=HEADERS)
     
@@ -73,7 +126,11 @@ def get_assets():
     r = requests.get(ASSETS_URL, headers=HEADERS)
     
     return json.loads(r.content)
-'''
+
+def init():
+    check_config()
+    save_assets()
+
 #response = create_order("MSFT", 1000, "buy", "market", "gtc")
 #response = create_order("AAPL", 100, "buy", "market", "gtc")
 
@@ -83,6 +140,6 @@ def get_assets():
 #print(orders)
 
 #print(str(assets))
-
-check_config()
-save_assets()
+init()
+print_all_stock_names()
+print_tradeable_stock_names()
--- utils.py
+++ utils.py
@@ -1,14 +1,17 @@
 from os import path
 
+DEFAULT_API_KEY = "YOUR_API_KEY_HERE"
+DEFAULT_SECRET_KEY = "YOUR_SECRET_KEY_HERE"
+
 def get_default_config():
     data = ""
     
     data += "# Api key" + "\n"
-    data += "API_KEY=YOUR_API_KEY_HERE" + "\n"
+    data += "API_KEY=" + DEFAULT_API_KEY + "\n"
     data += "\n"
     
     data += "# Secret key" + "\n"
-    data += "SECRET_KEY=YOUR_SECRET_KEY_HERE" + "\n"
+    data += "SECRET_KEY="+ DEFAULT_SECRET_KEY + "\n"
     data += "\n"
     
     return data
@@ -35,4 +38,32 @@ def read_file(filename):
     f = open(filename, "r")
     data = f.read()
     f.close()
+    
     return data
+    
+def get_lines_in_file(filename):
+    lines = []
+    
+    f = open(filename, "r")
+    
+    for line in f:
+        line = line.rstrip("\n\r")
+        if len(line) > 0:
+            if line[0] != '#':
+                lines.append(line)
+    
+    f.close()
+    
+    return lines
+    
+def get_value_from_config(filename, param):
+    lines = get_lines_in_file(filename)
+    
+    for line in lines:
+        if param in line:
+            return line.split(param + "=", 1)[1]
+            
+    return ""
+
+
+
